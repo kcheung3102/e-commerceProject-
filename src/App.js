787f47b64c47d0232e2cd88053ef7524ebc65846
Kevin Cheung <kcheung3102@gmail.com>
2019-09-28 00:11:43 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cart from './Components/Cart';
+import NavBar from './Components/NavBar';
+import ProductList from './Components/ProductList';
+import Error from './Components/Error';
+import Details from './Components/Details';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' component={ProductList} />
+        <Route path='/details' component={Details} />
+        <Route path='/cart' component={Cart} />
+        <Route component={Error} />
+      </Switch>
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,9 @@
+
+
+const Data = [
+  {
+    id: 1,
+    title: Jordan 1,
+    description: 
+  }
+]
