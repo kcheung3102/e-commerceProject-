c33937096c324bafc51c80a1b6b311e30de7a23d
Kevin Cheung <44646134+kcheung3102@users.noreply.github.com>
2019-09-29 22:20:04 -0400

    navbar progress

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../Logo.svg";
+import cart from "../iconfinder_Cart.svg";
 
 class NavBar extends Component {
-
   render() {
     return (
-      <nav className='navbar'>
+      <nav className="navbar">
+        <Link to="/">
+          <img src={logo} alt="shop logo" className="navbar-logo" />
+        </Link>
+        <ul className="navbar-nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
 
+          <li className="nav-item">
+            <Link to="/cart" className="nav-link">
+              <img src={cart} alt="cart logo" className="nav-bar-logo-cart" />
+              my cart
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
-
 }
 
 export default NavBar;
--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
 
-
-const Data = [
+ export const Data = [
   {
     id: 1,
-    title: Jordan 1,
-    description: 
+    title: 'Off-White x Air Jordan 1',
+    img:'images/off-white-air-jordan-1.jpg',
+    description:'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha',
+    inCart: false;
+    count: 0;
+    total: 0;
+  },
+  {
+    id: 2,
+    title: 'Off-White x Air Jordan 1',
+    img:'images/off-white-air-jordan-1.jpg',
+    description:'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha',
+    inCart: false;
+    count: 0;
+    total: 0;
+  },
+  {
+    id: 3,
+    title: 'Off-White x Air Jordan 1',
+    img:'images/off-white-air-jordan-1.jpg',
+    description:'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha',
+    inCart: false;
+    count: 0;
+    total: 0;
   }
 ]
