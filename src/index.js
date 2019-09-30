74094699b44701a56c82a577037d5c1c946e0c8e
Kevin Cheung <44646134+kcheung3102@users.noreply.github.com>
2019-09-30 00:16:07 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-<Router>
-  <App />
-</Router>
-, document.getElementById('root'));
-
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
