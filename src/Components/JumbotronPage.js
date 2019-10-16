8901704502e10056f7f8b9b63d965c33d78a0c03
Kcheung3102 <Kcheung3102@gmail.com>
2019-10-16 14:26:14 -0400

    commit

--- src/Components/JumbotronPage.js
+++ src/Components/JumbotronPage.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { MDBJumbotron, MDBContainer, MDBBtn, MDBCol, MDBRow,  } from "mdbreact";
+import { MDBJumbotron, MDBContainer, MDBBtn, MDBCol, MDBRow, MDBIcon  } from "mdbreact";
 import backgroundimg from "../images/off-white-air-jordan-1.jpg";
 
 const JumbotronPage = () => {
   return (
-    <MDBJumbotron fluid style={{ backgroundImage: 'url:({backgroundimg})'}}>
+    <MDBJumbotron fluid className="jumbo-bg">
       <MDBContainer>
-          <MDBCol>
-            <h2 className="display-4">Fluid jumbotron</h2>
+          <MDBCol className="text-white">
+            <h2 className="display-4">New Select Styles</h2>
             <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <MDBBtn outline color ="white" className="jumbo-btn">Shop now</MDBBtn>
           </MDBCol>
       </MDBContainer>
     </MDBJumbotron>
   )
 }
+
+
 
 
 
